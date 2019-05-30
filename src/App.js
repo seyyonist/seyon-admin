c2db5682639df76864a9560130513371879c1992
vijaykarthik n <nvijaykarthik@gmail.com>
2019-05-30 18:05:56 +0530

    Added router and menu details

--- src/App.js
+++ src/App.js
@@ -1,97 +1,31 @@
 import React from 'react';
 import Nav from "./Nav"
 import Sidebar from "./Sidebar";
+import { Switch, Route } from 'react-router-dom';
+import CompanyList from './CompanyList';
+import './App.css';
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={CompanyList} />
+    </Switch>
+  </main>
+)
 
 function App() {
   return (
-    <>
+    <div>
       <Nav />
       <div class="container-fluid page-body-wrapper">
         <Sidebar />
         <div class="main-panel">
           <div class="content-wrapper">
-            <div class="col-12 grid-margin">
-              <div class="card">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="card-body">
-                      <h4 class="card-title">Single color buttons</h4>
-                      <p class="card-description">Add class <code>.btn-&lt;color&lg;</code> for buttons in theme colors</p>
-                      <div class="template-demo">
-                        <button type="button" class="btn btn-primary">Primary</button>
-                        <button type="button" class="btn btn-secondary">Secondary</button>
-                        <button type="button" class="btn btn-success">Success</button>
-                        <button type="button" class="btn btn-danger">Danger</button>
-                        <button type="button" class="btn btn-warning">Warning</button>
-                        <button type="button" class="btn btn-info">Info</button>
-                        <button type="button" class="btn btn-light">Light</button>
-                        <button type="button" class="btn btn-dark">Dark</button>
-                        <button type="button" class="btn btn-link">Link</button>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="card-body">
-                      <h4 class="card-title">Rounded buttons</h4>
-                      <p class="card-description">Add class <code>.btn-rounded</code></p>
-                      <div class="template-demo">
-                        <button type="button" class="btn btn-primary btn-rounded btn-fw">Primary</button>
-                        <button type="button" class="btn btn-secondary btn-rounded btn-fw">Secondary</button>
-                        <button type="button" class="btn btn-success btn-rounded btn-fw">Success</button>
-                        <button type="button" class="btn btn-danger btn-rounded btn-fw">Danger</button>
-                        <button type="button" class="btn btn-warning btn-rounded btn-fw">Warning</button>
-                        <button type="button" class="btn btn-info btn-rounded btn-fw">Info</button>
-                        <button type="button" class="btn btn-light btn-rounded btn-fw">Light</button>
-                        <button type="button" class="btn btn-dark btn-rounded btn-fw">Dark</button>
-                        <button type="button" class="btn btn-link btn-rounded btn-fw">Link</button>
-                      </div> 
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12 stretch-card">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Default form</h4>
-                    <p class="card-description">
-                      Basic form layout
-                  </p>
-                    <form class="forms-sample">
-                      <div class="form-group">
-                        <label for="exampleInputUsername1">Username</label>
-                        <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username" />
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" />
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputConfirmPassword1">Confirm Password</label>
-                        <input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password" />
-                      </div>
-                      <div class="form-check form-check-flat form-check-primary">
-                        <label class="form-check-label">
-                          <input type="checkbox" class="form-check-input" />
-                          Remember me
-                      <i class="input-helper"></i></label>
-                      </div>
-                      <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                      <button class="btn btn-light">Cancel</button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Main />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
