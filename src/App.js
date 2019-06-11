7d0c25179b398c95f17b5dccadac549b1ec89e65
vijaykarthik n <nvijaykarthik@gmail.com>
2019-06-11 20:05:37 +0530

    company details

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Nav from "./Nav"
 import Sidebar from "./Sidebar";
 import { Switch, Route } from 'react-router-dom';
 import CompanyList from './CompanyList';
+import CompanyDetails from './CompanyDetails';
+
 import './App.css';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={CompanyList} />
+      <Route exact path="/companyDetails/:companyId" component={CompanyDetails} />
     </Switch>
   </main>
 )
---
+++ src/CompanyDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CompanyDetails extends Component {
+
+    render(){
+        return(
+            <h1>Company Details</h1>
+        )
+    }
+
+}
