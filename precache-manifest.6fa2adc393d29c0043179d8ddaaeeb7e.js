640ca4b916c932c96eec025c88c03ce3861bcfec
vijaykarthik n <nvijaykarthik@gmail.com>
2019-07-01 14:35:50 +0530

    initial commit for testing the working

---
+++ precache-manifest.6fa2adc393d29c0043179d8ddaaeeb7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2bbe1ecc12f6cd32294de3b7fdd394",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "39be560d0dcef64a51a6",
+    "url": "/admin/static/css/main.5446c57a.chunk.css"
+  },
+  {
+    "revision": "25aa9ac49a1b2fa6ed9a",
+    "url": "/admin/static/js/2.fe8fc715.chunk.js"
+  },
+  {
+    "revision": "39be560d0dcef64a51a6",
+    "url": "/admin/static/js/main.d9307ccd.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
